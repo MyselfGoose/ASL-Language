29df358de39366e96bf2f65455a87b40fcd9303d
Azeem Arshad <abubakar20069@gmail.com>
2024-10-15 23:23:33 +0500

    Added a form and the POST method

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let information = '';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,4 +19,9 @@ app.listen(port, () => {
 
 app.get("/", (req, res ) => {
   res.render(`${__dirname}/views/index.ejs`);
+});
+
+app.post('/submit', (req, res) =>{
+  information = req.body['information'];
+  console.log(`Information: ${information}`);
 });
