f3efde3f8e1be55541f9ef5a0192f6f076c0e14d
Azeem Arshad <abubakar20069@gmail.com>
2024-10-15 18:39:11 +0500

    Added views folder and a starting ejs file and made express render it

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("combined"));
 
@@ -13,5 +17,5 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res ) => {
-  
+  res.render(`${__dirname}/views/index.ejs`);
 });
